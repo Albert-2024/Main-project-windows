2a4bb2b10a0057369d2e70c3f8bdc33e4e236d11
Albert Devasia <albertdevasia2024@mca.ajce.in>
2024-01-24 09:24:37 +0530

    template changed as index, footer, header all products page

--- ElectrCOm/electrocom/myapp/views.py
+++ ElectrCOm/electrocom/myapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,"Ministore-1.0.0/Ministore-1.0.0/index.html")
+    return render(request,"index2.html")
 
 
     
